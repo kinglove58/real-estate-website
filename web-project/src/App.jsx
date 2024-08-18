7e500fe86d55adba3ffb5e83c674045074a81408
Techking <Kprincejah@gmail.com>
2024-08-18 18:06:08 +0100

    first commit

---
+++ web-project/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import Scroll from "./component/customscroll/Scroll";
+
+function App() {
+  return (
+    <>
+      <div>
+        <Scroll />
+      </div>
+    </>
+  );
+}
+
+export default App;
